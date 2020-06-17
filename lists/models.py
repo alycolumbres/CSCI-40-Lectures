0cbc06cfb8b1f86f6d712e5163779eb2944081ec
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-06-17 12:15:29 +0800

    Retyped models.py

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+	text = models.TextField(default='') # We are giving the text field a default via a keyword argument. / You're adding arguments to your methods when you're calling them, but you give a name to those arguments.
